ae5026cc13664e7b491d688aa211eabb9f0dff3b
Tyrone Parsons <Tyrone.Parsons@signature.co.za>
2021-01-07 05:06:46 +0200

    Anagram challenge logic implemented and anagram unit tests amended.

--- TGS.Challenge/Anagram.cs
+++ TGS.Challenge/Anagram.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace TGS.Challenge
 {
-  /*
+    /*
         Devise a function that checks if 1 word is an anagram of another, if the words are anagrams of
         one another return true, else return false
 
@@ -15,16 +19,43 @@ namespace TGS.Challenge
 
         horse!! shore = true
         horse  !! shore = true
-          horse? heroes = true
+        horse? heroes = true
 
         There are accompanying unit tests for this exercise, ensure all tests pass & make
         sure the unit tests are correct too.
-     */
+    */
+
     public class Anagram
     {
-      public bool AreAnagrams(string word1, string word2)
-      {
-        return false;
-      }
+        public bool AreAnagrams(string word1, string word2)
+        {
+            /* ensure that both given arguments are not null or empty */
+            if (string.IsNullOrEmpty(word1) || string.IsNullOrEmpty(word2))
+            {
+                throw new ArgumentException();
+            }
+
+            /* create temporary list and check off found characters until either a character is not found or all are removed */
+            List<char> characters = word1.ToLowerInvariant()
+                .ToList();
+
+            foreach (char c in word2)
+            {
+                /* ignore punctuation & whitespace */
+                if (char.IsPunctuation(c) || char.IsWhiteSpace(c))
+                {
+                    continue;
+                }
+
+                /* try and remove the given character from remaining characters */
+                if (!characters.Remove(char.ToLowerInvariant(c)))
+                {
+                    return false;
+                }
+            }
+
+            /* return true if all characters found */
+            return true;
+        }
     }
 }
