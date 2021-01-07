bf36b4bb0cbc9e2a8d68fe95215b91728ddc5bba
Tyrone Parsons <Tyrone.Parsons@signature.co.za>
2021-01-07 06:00:13 +0200

    Vowel count logic implemented and unit tests amended.

--- TGS.Challenge/VowelCount.cs
+++ TGS.Challenge/VowelCount.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace TGS.Challenge
 {
@@ -11,12 +12,29 @@ namespace TGS.Challenge
 
         There are accompanying unit tests for this exercise, ensure all tests pass & make
         sure the unit tests are correct too.
-     */
+    */
+
     public class VowelCount
     {
+        /* list of lowercase vowels */
+        private readonly string vowels = "aeiou";
+
         public int Count(string value)
         {
-            return -1;
+            /* ensure given value is not empty */
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException();
+            }
+
+            /* return the count of vowels in the given string */
+            return value.Count(c => IsVowel(c));
+        }
+
+        /* checks if the given character is a vowel */
+        private bool IsVowel(char c)
+        {
+            return vowels.Contains(char.ToLowerInvariant(c));
         }
     }
 }
