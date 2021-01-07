6dd7ccc25dfbf77c952724b410efafff0e5bedab
Tyrone Parsons <Tyrone.Parsons@signature.co.za>
2021-01-07 05:14:50 +0200

    Equivalence index logic implemented and unit tests amended.

--- TGS.Challenge.Tests/EquivalenceIndexTests.cs
+++ TGS.Challenge.Tests/EquivalenceIndexTests.cs
@@ -2,29 +2,29 @@ using Xunit;
 
 namespace TGS.Challenge.Tests
 {
-  public class EquivalenceIndexTests
-  {
-    private readonly EquivalenceIndex _equivalenceIndex;
-
-    public EquivalenceIndexTests()
+    public class EquivalenceIndexTests
     {
-      this._equivalenceIndex= new EquivalenceIndex();
+        private readonly EquivalenceIndex _equivalenceIndex;
+
+        public EquivalenceIndexTests()
+        {
+            _equivalenceIndex = new EquivalenceIndex();
+        }
+
+        [Fact]
+        public void Returns_Index_ForValidNumberSequence()
+        {
+            var index = _equivalenceIndex.Find(new int[] { 1, 2, 3, 4, 5, 7, 8, 10, 12 });
+
+            Assert.Equal(6, index);
+        }
+
+        [Fact]
+        public void Retruns_Neg1_ForInvalidNumberSequence()
+        {
+            var index = _equivalenceIndex.Find(new int[] { 2, 2, 3, 4, 3, 74, 8, 10, 12 });
+
+            Assert.Equal(-1, index);
+        }
     }
-
-    [Fact]
-    public void Returns_Index_ForValidNumberSequence()
-    {
-        var index = _equivalenceIndex.Find(new int[] { 1, 2, 3, 4, 5, 7, 8, 10, 12 });
-
-        Assert.Equal(index, 6);
-    }
-
-    [Fact]
-    public void Retruns_Neg1_ForInvalidNumberSequence()
-    {
-      var index = _equivalenceIndex.Find(new int[] { 2, 2, 3, 4, 3, 74, 8, 10, 12 });
-
-      Assert.Equal(index, -1);
-    }
-  }  
 }
--- TGS.Challenge/EquivalenceIndex.cs
+++ TGS.Challenge/EquivalenceIndex.cs
@@ -1,25 +1,54 @@
+using System;
+using System.Linq;
+
 namespace TGS.Challenge
 {
-  /*
-       Given a zero-based integer array of length N, the equivalence index (i) is the index where the sum of all the items to the left of the index
-       are equal to the sum of all the items to the right of the index.
+    /*
+        Given a zero-based integer array of length N, the equivalence index (i) is the index where the sum of all the items to the left of the index
+        are equal to the sum of all the items to the right of the index.
 
-       Constraints: 0 <= N <= 100 000
+        Constraints: 0 <= N <= 100 000
 
-       Example: Given the following array [1, 2, 3, 4, 5, 7, 8, 10, 12]
-       Your program should output "6" because 1 + 2 + 3 + 4 + 5 + 7 = 10 + 12
+        Example: Given the following array [1, 2, 3, 4, 5, 7, 8, 10, 12]
+        Your program should output "6" because 1 + 2 + 3 + 4 + 5 + 7 = 10 + 12
 
-       If no index exists then output -1
+        If no index exists then output -1
 
-       There are accompanying unit tests for this exercise, ensure all tests pass & make
+        There are accompanying unit tests for this exercise, ensure all tests pass & make
         sure the unit tests are correct too.
-     */
+    */
 
     public class EquivalenceIndex
     {
-      public int Find(int[] numbers)
-      {
-        return -99;
-      }
+        /* the maximum arrary length constraint */
+        private readonly int maxArrayLength = 100000;
+
+        public int Find(int[] numbers)
+        {
+            /* ensure the given array length does not exceed the length constraint */
+            if (numbers.Length > maxArrayLength)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            /* sum the total number and keep a running total of what has been added on the left and removed from the right */
+            int total = numbers.Sum();
+            int current = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                total -= numbers[i];
+
+                if (current == total)
+                {
+                    return i;
+                }
+
+                current += numbers[i];
+            }
+
+            /* no index found */
+            return -1;
+        }
     }
 }
